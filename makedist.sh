5d8034fd82f6b08747e012954587f846760d401c
Richard Boulton <richard@tartarus.org>
2006-06-11 19:25:04 +0000

    Now able to make a distribution.
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/pystemmer@376 633ccae0-01f4-0310-8c99-d3591da6f01f

---
+++ makedist.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+epydoc --html -o docs/html --check Stemmer.so
+rm -fr dist
+python setup.py sdist
+(cd dist &&
+ tar zxf PyStemmer*.tar.gz &&
+ cd `find -type d|head -n 2|tail -n 1` &&
+ python setup.py install --install-lib=`pwd` &&
+ python setup.py sdist &&
+ python runtests.py
+)
--- setup.py
+++ setup.py
@@ -9,32 +9,52 @@ try:
 except:
     have_pyrex = 0
 
+# Directory which libstemmer sources are unpacked in.
 library_dir = 'libstemmer_c'
+# Directories in libstemmer which contain libstemmer sources (ie, not
+# examples, etc).
 library_core_dirs = set(('src_c', 'runtime', 'libstemmer', 'include'))
 
-# Read the manifest of files:
+# Read the manifest of files in libstemmer.
 src_files = [os.path.join(library_dir, line.strip())
              for line in open(os.path.join(library_dir, 'MANIFEST'))
              if len(line.strip()) > 2
              and line.strip()[-2:] == '.c'
              and os.path.split(line.strip())[0] in library_core_dirs]
-
-# Set the include path
+# Set the include path to include libstemmer.
 include_dirs = ('src', os.path.join(library_dir, 'include'))
 
-# Add the pyrex stuff.
 if have_pyrex:
+    # Add the pyrex sources, and a special rule so distutils knows how to
+    # use them.
     src_files.append('src/Stemmer.pyx')
     cmdclass = {'build_ext': build_ext}
 else:
+    # Add just the C sources.
     src_files.append('src/Stemmer.c')
     cmdclass = {}
 
 setup(name = 'PyStemmer',
       version = '0.9',
-      description = 'Snowball stemming algorithms',
       author = 'Richard Boulton',
       author_email = 'richard@tartarus.org',
+      maintainer = 'Richard Boulton',
+      maintainer_email = 'richard@tartarus.org',
+      url = 'http://snowball.tartarus.org/',
+      description = 'Snowball stemming algorithms, for information retrieval',
+      long_description = """
+      PyStemmer provides access to efficient algorithms for calculating a
+      "stemmed" form of a word.  This is a form with most of the common
+      morphological endings removed; hopefully representing a common linguistic
+      base form.  This is most useful in building search engines and
+      information retrieval software; for example, a search with stemming
+      enabled should be able to find a document containing "cycling" given the
+      query "cycles".
+      
+      PyStemmer provides algorithms for several (mainly european) languages,
+      by wrapping the libstemmer library from the Snowball project in a
+      Python module.
+      """
 
       ext_modules = [Extension('Stemmer', src_files,
                                include_dirs = include_dirs)],
