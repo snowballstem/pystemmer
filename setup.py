b8b4f9d4016a7e660b97e3d9232cef81e9d25443
Richard Boulton <richard@tartarus.org>
2006-06-11 11:39:32 +0000

    Should work without pyrex, if the generated .c file is present.
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/pystemmer@374 633ccae0-01f4-0310-8c99-d3591da6f01f

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-from Pyrex.Distutils import build_ext
 import os.path
+
+try:
+    from Pyrex.Distutils import build_ext
+    have_pyrex = 1
+except:
+    have_pyrex = 0
 
 library_dir = 'libstemmer_c'
 library_core_dirs = set(('src_c', 'runtime', 'libstemmer', 'include'))
@@ -13,8 +18,17 @@ src_files = [os.path.join(library_dir, line.strip())
              if len(line.strip()) > 2
              and line.strip()[-2:] == '.c'
              and os.path.split(line.strip())[0] in library_core_dirs]
-src_files.append('src/Stemmer.pyx')
+
+# Set the include path
 include_dirs = ('src', os.path.join(library_dir, 'include'))
+
+# Add the pyrex stuff.
+if have_pyrex:
+    src_files.append('src/Stemmer.pyx')
+    cmdclass = {'build_ext': build_ext}
+else:
+    src_files.append('src/Stemmer.c')
+    cmdclass = {}
 
 setup(name = 'PyStemmer',
       version = '0.9',
@@ -24,5 +38,5 @@ setup(name = 'PyStemmer',
 
       ext_modules = [Extension('Stemmer', src_files,
                                include_dirs = include_dirs)],
-      cmdclass = {'build_ext': build_ext}
+      cmdclass = cmdclass
      )
