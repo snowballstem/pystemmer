b0638addd1ec9dd2fe06f4380f8ca433562a75ac
Avikam Agur <avikam@pagaya-inv.com>
2018-06-17 15:34:10 +0300

    moved tarballfetcher inside the setup file

--- setup.py
+++ setup.py
@@ -2,12 +2,25 @@
 from distutils.core import setup, Extension
 from distutils.cmd import Command
 import os.path
+import hashlib
+import os
+import sys
+import tarfile
+
+try:
+    from urllib import urlretrieve
+    from urlparse import urlparse
+except ImportError:
+    from urllib.request import urlretrieve
+    from urllib.parse import urlparse
 
 try:
     from Cython.Distutils import build_ext
+
     have_pyrex = 1
 except ImportError:
     from distutils.command.build_ext import build_ext
+
     have_pyrex = 0
 
 # Directory which libstemmer sources are unpacked in.
@@ -29,9 +42,43 @@ class BootstrapCommand(Command):
     def finalize_options(self):
         pass
 
+    def download_file(self, url, filename):
+        sys.stdout.write('Downloading %s... ' % url)
+        sys.stdout.flush()
+        urlretrieve(url, filename)
+        sys.stdout.write('DONE\n')
+
+    def extract_tarball(self, tarball_filename):
+        tarball = tarfile.open(tarball_filename, 'r:gz')
+        sys.stdout.write('Extracting %s... ' % tarball_filename)
+        sys.stdout.flush()
+        tarball.extractall('.')
+        sys.stdout.write('DONE\n')
+
+    def md5_file(self, filename):
+        return hashlib.md5(open(filename, 'rb').read()).hexdigest()
+
+    def download_and_extract_tarball(self, tarball_url, tarball_filename=None, expected_md5=None):
+        if tarball_filename is None:
+            tarball_filename = os.path.basename(urlparse(tarball_url).path)
+
+        if not os.path.exists(tarball_filename):
+            self.download_file(tarball_url, tarball_filename)
+
+        if not expected_md5 is None:
+            sys.stdout.write('Checking that MD5 of %s is %s... ' % (tarball_filename, expected_md5))
+            actual_md5 = self.md5_file(tarball_filename)
+            sys.stdout.write('MD5 is %s. ' % actual_md5)
+            if actual_md5 == expected_md5:
+                sys.stdout.write('OK\n')
+            else:
+                sys.stdout.write('Incorrect MD5!\n')
+                sys.exit(1)
+
+        self.extract_tarball(tarball_filename)
+
     def run(self):
-        from tarballfetcher import download_and_extract_tarball
-        download_and_extract_tarball(
+        self.download_and_extract_tarball(
             self.libstemmer_url, expected_md5=self.libstemmer_md5)
 
 
@@ -42,10 +89,10 @@ class BuildExtCommand(build_ext):
 
         # Read the manifest of files in libstemmer.
         src_files.extend([os.path.join(library_dir, line.strip().replace(' \\', ''))
-                     for line in open(os.path.join(library_dir, 'mkinc_utf8.mak'))
-                     if len(line.strip()) > 2
-                     and (line.strip().endswith('.c \\') or line.strip().endswith('.c'))
-                     and os.path.split(line.strip().replace(' \\', ''))[0] in library_core_dirs])
+                          for line in open(os.path.join(library_dir, 'mkinc_utf8.mak'))
+                          if len(line.strip()) > 2
+                          and (line.strip().endswith('.c \\') or line.strip().endswith('.c'))
+                          and os.path.split(line.strip().replace(' \\', ''))[0] in library_core_dirs])
 
         build_ext.run(self)
 
@@ -53,7 +100,6 @@ class BuildExtCommand(build_ext):
 # Directories in libstemmer which contain libstemmer sources (ie, not
 # examples, etc).
 library_core_dirs = ('src_c', 'runtime', 'libstemmer', 'include')
-
 
 # Set the include path to include libstemmer.
 include_dirs = ('src', os.path.join(library_dir, 'include'))
