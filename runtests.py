b2826f19fe8ba65238b5f3b4cee7096a698f048e
Felix Yan <felixonmars@archlinux.org>
2022-11-29 06:43:08 +0200

    Fix doctests
    The renamed quickstart docs were not updated accordingly in runtests.py,
    and quickstart_python2.txt should be in the docs subdir.

--- runtests.py
+++ runtests.py
@@ -6,9 +6,9 @@ import sys
 py3k = sys.version_info >= (3, 0)
 
 if py3k:
-    num_failures, num_tests = doctest.testfile('docs/quickstart_python3.txt')
-else:
     num_failures, num_tests = doctest.testfile('docs/quickstart.txt')
+else:
+    num_failures, num_tests = doctest.testfile('docs/quickstart_python2.txt')
 
 if num_failures > 0:
     print("%d failures out of %d tests" % (num_failures, num_tests))
